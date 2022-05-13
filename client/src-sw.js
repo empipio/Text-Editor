9cb95791c9924d566bbcf23f69210be221593ffa
empipio <94937507+empipio@users.noreply.github.com>
2022-05-13 14:51:56 +0100

    comments added

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,7 +28,7 @@ registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  // Here we define the callback function that will filter the requests we want to cache (JS and CSS files in this instance)
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
     // Name of the cache storage.
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Method to accept content and add it to the database
 export const putDb = async (content) => {
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readwrite");
@@ -22,7 +22,7 @@ export const putDb = async (content) => {
   console.log("Data successfully saved to database!", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Method to get all the content from the database
 export const getDb = async () => {
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readonly");
