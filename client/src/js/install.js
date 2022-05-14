1ce16e1420ef6d35101c39ed5d84a6981907ff58
empipio <94937507+empipio@users.noreply.github.com>
2022-05-14 23:07:39 +0100

    todos removed

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,6 +1,7 @@
 const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
+
 // Event handler for the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
   // Store the triggered events
